ae981110e1358b523c7914e8d9fdceb6a46b7a2c
kaypee90 <kaypee90@yahoo.com>
2023-02-23 18:16:10 +0000

    Add cookie excepts

--- src/Services/Identity/Identity.API/Startup.cs
+++ src/Services/Identity/Identity.API/Startup.cs
@@ -76,6 +76,10 @@ namespace Identity.API
 
             app.UseCors();
 
+            app.UseCookiePolicy(new CookiePolicyOptions
+            {
+                MinimumSameSitePolicy = SameSiteMode.None, Secure = CookieSecurePolicy.Always,
+            });
 
             app.UseStaticFiles();
 
